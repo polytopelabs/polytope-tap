175be628343f27728f65a406544adae1d4f712ac
André Eriksson <aeriksson@users.noreply.github.com>
2024-03-14 17:28:51 +0100

    Update Polytope CLI to release 0.1.20 (e5c165e13)

--- Formula/polytope-cli.rb
+++ Formula/polytope-cli.rb
@@ -1,35 +1,35 @@
 class PolytopeCli < Formula
   desc "The Polytope CLI"
   homepage "https://polytope.com"
-  version "0.1.19"
+  version "0.1.20"
 
   @@os = "#{OS.mac? ? 'macos' : 'linux'}"
   @@arch = "#{Hardware::CPU.intel? ? 'amd64' : 'arm64'}"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://polytope-cli-releases.s3.eu-north-1.amazonaws.com/pt-0.1.19-macos-amd64.xz"
-      sha256 "fbcf8413394b5b778a919ffa179eeabf162f3ba17ca644aa18e2b5bad42d4cd3"
+      url "https://polytope-cli-releases.s3.eu-north-1.amazonaws.com/pt-0.1.20-macos-amd64.xz"
+      sha256 "b8d608021efbe6ea35b0c87569ffdec3f10402382ddb95e35b66ee67acf43d4d"
     elsif Hardware::CPU.arm?
-      url "https://polytope-cli-releases.s3.eu-north-1.amazonaws.com/pt-0.1.19-macos-arm64.xz"
-      sha256 "af55157bc1e87f8c62836aa4d94983651ba8e363a3c5328e5a7bf411d1039cdd"
+      url "https://polytope-cli-releases.s3.eu-north-1.amazonaws.com/pt-0.1.20-macos-arm64.xz"
+      sha256 "f2c5d9a54020792e5b1211c1077521a41592e16b716ef260d116ec84229493eb"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://polytope-cli-releases.s3.eu-north-1.amazonaws.com/pt-0.1.19-linux-amd64.xz"
-      sha256 "05cc9b58e00b25c423af0ec0cc5a881fcec37896fc01b1ff48d9730488f3dc95"
+      url "https://polytope-cli-releases.s3.eu-north-1.amazonaws.com/pt-0.1.20-linux-amd64.xz"
+      sha256 "875e814906b67d3643aeb004f55ef9834dffbac421a9ffef289d8ff3e946d92f"
     elsif Hardware::CPU.arm?
       odie "We don't provide ARM builds on Linux yet – please let us know if you want this!"
     end
   end
 
   def install
-    bin.install "pt-0.1.19-#{@@os}-#{@@arch}" => "pt"
+    bin.install "pt-0.1.20-#{@@os}-#{@@arch}" => "pt"
   end
 
   test do
-    assert_match "0.1.19-42c86a4ec-#{@@os}-#{@@arch}", shell_output("#{bin}/pt --version")
+    assert_includes shell_output("#{bin}/pt --version 2>&1"), "0.1.20-e5c165e13-#{@@os}-#{@@arch}"
   end
 end

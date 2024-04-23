f6c545ec3034a190590f7a44d11ce32da513962a
André Eriksson <aeriksson@users.noreply.github.com>
2024-04-23 11:35:21 +0200

    Update Polytope CLI to release 0.1.26 (73f1b2a35)

--- Formula/polytope-cli.rb
+++ Formula/polytope-cli.rb
@@ -8,17 +8,17 @@ class PolytopeCli < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://polytope-cli-releases.s3.eu-north-1.amazonaws.com/pt-0.1.26-macos-amd64.gzip"
+      url "https://polytope-cli-releases.s3.eu-north-1.amazonaws.com/pt-0.1.26-macos-amd64.gz"
       sha256 "4b04e30c34f67e27c4d65892d4b3409758a1c55268b44763259dfab346651636"
     elsif Hardware::CPU.arm?
-      url "https://polytope-cli-releases.s3.eu-north-1.amazonaws.com/pt-0.1.26-macos-arm64.gzip"
+      url "https://polytope-cli-releases.s3.eu-north-1.amazonaws.com/pt-0.1.26-macos-arm64.gz"
       sha256 "d72e761fef7bd6daa78dcece4c928a1a42273c19854135eb4e21f19cddcf5e47"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://polytope-cli-releases.s3.eu-north-1.amazonaws.com/pt-0.1.26-linux-amd64.gzip"
+      url "https://polytope-cli-releases.s3.eu-north-1.amazonaws.com/pt-0.1.26-linux-amd64.gz"
       sha256 "16ef83dce7e221b1feb3296acea7d3ce01aed5c0cf9aae89f368328130e4ba50"
     elsif Hardware::CPU.arm?
       odie "We don't provide ARM builds on Linux yet – please let us know if you want this!"

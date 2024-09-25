ca6b2f9aba3c26b3047c1334e5263d182bc7535a
André Eriksson <aeriksson@users.noreply.github.com>
2024-09-25 11:42:26 +0200

    Update Polytope CLI to release 0.1.31 (bae4935de)

--- Formula/polytope-cli.rb
+++ Formula/polytope-cli.rb
@@ -1,35 +1,36 @@
 class PolytopeCli < Formula
   desc "The Polytope CLI"
   homepage "https://polytope.com"
-  version "0.1.29"
+  version "0.1.31"
 
   @@os = "#{OS.mac? ? 'macos' : 'linux'}"
   @@arch = "#{Hardware::CPU.intel? ? 'amd64' : 'arm64'}"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://polytope.com/releases/polytope-cli-0.1.29-macos-amd64.gz"
-      sha256 "e189bdd34608d78bce7ee6649bf8d86f8094f65f95e74f6fb6a9f25dcf543f08"
+      url "https://polytope.com/releases/polytope-cli-0.1.31-macos-amd64.gz"
+      sha256 "727f382c7f76c2fb6529b0cde0bdf4e9c96e25086df911f0baa75a5c42a10bea"
     elsif Hardware::CPU.arm?
-      url "https://polytope.com/releases/polytope-cli-0.1.29-macos-arm64.gz"
-      sha256 "31b84e9fddbfb5fd1c3cf4152f494b93c07fb9867d8aacee47e712ea5efd694e"
+      url "https://polytope.com/releases/polytope-cli-0.1.31-macos-arm64.gz"
+      sha256 "1e2f13ac4cbf89cebe892d3434a892b7230b8877b054ca1383728d68e00a2dfd"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://polytope.com/releases/polytope-cli-0.1.29-linux-amd64.gz"
-      sha256 "0c7054d93e4791453696abada713f852134c9d91b042e1ce9181ceba93fe6d2c"
+      url "https://polytope.com/releases/polytope-cli-0.1.31-linux-amd64.gz"
+      sha256 "5d888bfa4533ceac22a6ad59b8297c95480c17997da129de301ad271bedffc0c"
     elsif Hardware::CPU.arm?
       odie "We don't provide ARM builds on Linux yet – please let us know if you want this!"
     end
   end
 
   def install
-    bin.install "polytope-cli-0.1.29-#{@@os}-#{@@arch}" => "pt"
+    bin.install "polytope-cli-0.1.31-#{@@os}-#{@@arch}" => "polytope"
+    bin.install_symlink "polytope" => "pt"
   end
 
   test do
-    assert_includes shell_output("#{bin}/pt --version 2>&1"), "0.1.29-106cc9541-#{@@os}-#{@@arch}"
+    assert_includes shell_output("#{bin}/polytope --version 2>&1"), "0.1.31-bae4935de-#{@@os}-#{@@arch}"
   end
 end
